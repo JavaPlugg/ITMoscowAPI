3a0cad67a599bc11941321ff28826b465376f60d
JavaPlugg <tish15.123@gmail.com>
2026-01-03 19:42:40 +0300

    pom fix

---
+++ ITMoscowAPIServer/src/main/java/net/javaplugg/itmoscow/api/server/exception/CannotApplyReplacementsException.java
@@ -0,0 +1,7 @@
+package net.javaplugg.itmoscow.api.server.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class CannotApplyReplacementsException extends RuntimeException {
+}
